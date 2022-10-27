1d26f881f0508c1b4f20aa4e92b4622b48be56c8
Bob <rjwhitmercc@gmail.com>
2022-10-27 11:16:11 -0600

    Create player show route with player information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,55 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
   def index
     @users = User.all
   end
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
-  def create; end
+  def show
+    @full_name = "#{@user.first_name} #{@user.last_name}"
+    @games = PlayerGame.where(user: @user)
+  end
 
-  def update; end
+  def create
+    @user = User.new(user_params)
 
-  def destroy; end
+    @user.transaction do
+      @user.save!
+    end
+    redirect_to :index
+  rescue ActiveRecord::RecordInvalid
+    render :new, status: :unprocessable_entity
+  end
+
+  def update
+    @user.update(user_params)
+
+    @user.transaction do
+      @user.save!
+    end
+  rescue ActiveRecord::RecordInvalid
+    render :update, status: :unprocessable_entity
+  end
+
+  def destroy
+    @user.destroy!
+
+    render :index, notice[:flash] = 'User was successfully deleted'
+  end
 
   private
 
   def user_params
-    params.require(:team).permit(:first_name, :last_name, :phone_number, :email)
+    params.permit(:first_name, :last_name, :phone_number, :email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
